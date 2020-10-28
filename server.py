5ddd609a2bd96048c3233358c58cfc8d703578a1
Ashley Qin <ashley.qin@yale.edu>
2020-10-28 12:43:18 -0700

    trying to make deployment work

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 
 from utils import get_phenolist, get_gene_tuples, pad_gene, PheWebError
-from .conf_utils import conf
-from .file_utils import common_filepaths
-from .server_utils import get_variant, get_random_page, get_pheno_region
-from .autocomplete import Autocompleter
-from .auth import GoogleSignIn
-from .version import version as pheweb_version
-from .import weetabix
+from conf_utils import conf
+from file_utils import common_filepaths
+from server_utils import get_variant, get_random_page, get_pheno_region
+from autocomplete import Autocompleter
+from auth import GoogleSignIn
+from version import version as pheweb_version
+from weetabix import *
+#from import weetabix
 
 from flask import Flask, jsonify, render_template, request, redirect, abort, flash, send_from_directory, send_file, session, url_for, Blueprint
 from flask_compress import Compress
