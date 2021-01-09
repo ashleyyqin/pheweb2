31bb39c11577a3cb4f9efa771dbb60a9e7791f96
Ashley Qin <43159850+ashleyyqin@users.noreply.github.com>
2021-01-09 11:40:01 -0800

    Trying to make "send_file" path not relative

--- server.py
+++ server.py
@@ -134,7 +134,8 @@ def phenotypes_page():
 @bp.route('/api/phenotypes.json')
 @check_auth
 def api_phenotypes():
-    return send_file(common_filepaths['phenotypes_summary']())
+    return redirect(common_filepaths['phenotypes_summary']())
+    #return send_file(common_filepaths['phenotypes_summary']())
 
 @bp.route('/api/qq/pheno/<phenocode>')
 @check_auth
